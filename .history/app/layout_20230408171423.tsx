21ea81d35d8e69a3200f6c2c55e65948aff9eade
Yuba Neupane <yjn5031@psu.edu>
2023-04-08 18:03:41 -0400

    makeing the modal for login

--- .history/app/layout_20230408171423.tsx
+++ .history/app/layout_20230408171423.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen title={"Hello World"} actionLabel="Submit" />
+          <Modal isOpen />
           <Navbar />
         </ClientOnly>
         {children}
